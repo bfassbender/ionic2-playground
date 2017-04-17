1bcd92a70c97d9bc55d88eb76f6f5b8ec80ee2e3
B. Faßbender <serrin@posteo.de>
2017-04-18 00:34:21 +0200

    Changed min Android Version to 4.4 Intro redone to fix flickering when transitioning from intro end to app on android

--- src/pages/introduction/introduction.ts
+++ src/pages/introduction/introduction.ts
@@ -1,16 +1,10 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, Slides } from 'ionic-angular';
 
 import { MainPage } from '../pages';
 
 import { SettingsProvider } from '../../providers/settings-provider';
 import { GoogleAnalyticsTracker} from '../../providers/google-analytics-tracker';
-
-export interface Slide {
-  title: string;
-  description: string;
-  image: string;
-}
 
 @Component({
   selector: 'page-introduction',
@@ -18,30 +12,30 @@ export interface Slide {
 })
 export class IntroductionPage {
 
-  slides: Slide[];
   showSkip = true;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private settingsProv: SettingsProvider, private gaTracker: GoogleAnalyticsTracker) {
-    this.slides = [
-      {
-        title: "<b>Teile Deine Schnappschüsse</b> im Portraitarchiv",
-        description: "Mit Snapshare kannst Du Deine Fotos einfach und bequem direkt von Deinem Smartphone auf <a href='https://www.portrait-archiv.com'>portrait-archiv.com</a> teilen.",
-        image: 'assets/img/intro_slide_1.png',
-      }
-    ];
-  }
+  @ViewChild('slides') slides: Slides;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private settingsProv: SettingsProvider, 
+    private gaTracker: GoogleAnalyticsTracker
+  ) { }
 
   startApp() {
-    this.settingsProv.setIntroShown(true);
-    this.navCtrl.setRoot(MainPage, {}, {
-      animate: true,
-      direction: 'forward'
+    this.navCtrl.setRoot(MainPage).then(() => {
+      this.settingsProv.setIntroShown(true);
     });
   }
 
-  onSlideChangeStart(slider) {
-    this.showSkip = !slider.isEnd;
+  onSlideChangeStart(slides: Slides) {
+    this.showSkip = !slides.isEnd();
   }
+	
+  ionViewWillEnter() {
+		this.slides.update();
+	}
 
   ionViewDidEnter() {
     this.gaTracker.trackView("Introduction Page");
