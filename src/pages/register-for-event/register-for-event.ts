5c37807b7fa2b2268f8e99d94bcc53f3d5fb2ed8
Bernd Faßbender <serrin@posteo.de>
2017-09-04 20:37:17 +0200

    Moved event registration to separate page in order to reuse it from intro as well as settings

--- src/pages/register-for-event/register-for-event.ts
+++ src/pages/register-for-event/register-for-event.ts
@@ -1,8 +1,10 @@
 import { Component, ViewChild } from '@angular/core';
-import { Slides, NavController, NavParams, TextInput } from 'ionic-angular';
+import { Slides, NavController, NavParams, TextInput, App} from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { createVeranstaltungsCodeValidator } from '../../validators/gallery-code-validator';
 import { PortraitArchivApiProvider } from '../../providers/portrait-archiv-api/portrait-archiv-api';
+import { SettingsProvider } from '../../providers/settings-provider'
+import { TabsPage } from '../tabs/tabs'
 
 @Component({
   selector: 'page-register-for-event',
@@ -18,11 +20,19 @@ export class RegisterForEventPage {
   userNameForm: FormGroup;
 
   eventName: String;
+
+  settings: any;
   
 
-  constructor(navCtrl: NavController, navParams: NavParams, formBuilder: FormBuilder, private apiProvider: PortraitArchivApiProvider) {
+  constructor(
+      navCtrl: NavController, 
+      navParams: NavParams, 
+      formBuilder: FormBuilder, 
+      private apiProvider: PortraitArchivApiProvider, 
+      private settingsProvider : SettingsProvider,
+      public appCtrl: App) {
     this.eventCodeForm = formBuilder.group({
-      veranstaltungsCode: ['', Validators.compose([Validators.required, Validators.pattern('[0-9]{5}-[0-9]{4}-[0-9]{4}')]), createVeranstaltungsCodeValidator(apiProvider,this)],
+      eventCode: ['', Validators.compose([Validators.required, Validators.pattern('[0-9]{5}-[0-9]{4}-[0-9]{4}')]), createVeranstaltungsCodeValidator(apiProvider,this)],
     });
 
     this.userNameForm = formBuilder.group({
@@ -31,13 +41,25 @@ export class RegisterForEventPage {
 
   }
 
-   ngOnInit() {
-    
+  ngOnInit() {
+
+    this.settingsProvider.loadSettings().then(settings => {
+      if(settings) {
+        console.info(this.constructor.name + ": Current settings - " + JSON.stringify(settings));
+        this.settings = settings;
+        this.eventCodeForm.get('eventCode').setValue(this.settings.eventCode,{emitEvent:false});
+        this.userNameForm.get('userName').setValue(this.settings.userName,{emitEvent:false});
+      }
+      else {
+        console.info("No settings found in storage");
+      }
+    });
+
     this.eventCodeForm.statusChanges
         .debounceTime(400)
         .subscribe(status => {
           if(status === 'VALID') {
-            this.apiProvider.ladeGalerie(this.eventCodeForm.value.veranstaltungsCode).subscribe(data => {
+            this.apiProvider.ladeGalerie(this.eventCodeForm.value.eventCode).subscribe(data => {
               this.eventName = data.galerie.title;
               this.eventCodeInput.setBlur();
               this.next();
@@ -46,10 +68,6 @@ export class RegisterForEventPage {
         });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterForEventPage');
-  }
-  
   next() {
     this.signupSlider.slideNext();
   }
@@ -62,16 +80,22 @@ export class RegisterForEventPage {
     this.submitAttempt = true;
  
     if(!this.eventCodeForm.valid){
-        this.signupSlider.slideTo(0);
+      this.signupSlider.slideTo(0);
     } 
     else if(!this.userNameForm.valid){
-        this.signupSlider.slideTo(1);
+      this.signupSlider.slideTo(1);
     }
     else {
-        console.log("success!")
-        console.log(this.eventCodeForm.value);
-        console.log(this.userNameForm.value);
+        this.settingsProvider.saveSettings({
+          "userName" : this.userNameForm.value.userName,
+          "eventCode" : this.eventCodeForm.value.eventCode
+        });
+        this.finishWorkflow();
     }
+  }
+
+  private finishWorkflow() {
+    this.appCtrl.getRootNav().setRoot(TabsPage);
   }
 
 }
