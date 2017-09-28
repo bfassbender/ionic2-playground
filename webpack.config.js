8e92af735ed3a3c92036006812757ebc4fc73419
bfassbender <serrin@posteo.de>
2017-09-28 23:00:09 +0200

    Updated to ionic-angular 3.7, angular 4.4.3, ionic-native 4.3.0 and ionic-app-scripts 3.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,68 @@
+/*
+ * The webpack config exports an object that has a valid webpack configuration
+ * For each environment name. By default, there are two Ionic environments:
+ * "dev" and "prod". As such, the webpack.config.js exports a dictionary object
+ * with "keys" for "dev" and "prod", where the value is a valid webpack configuration
+ * For details on configuring webpack, see their documentation here
+ * https://webpack.js.org/configuration/
+ */
+
 var path = require('path');
 var webpack = require('webpack');
 var ionicWebpackFactory = require(process.env.IONIC_WEBPACK_FACTORY);
 
 var ModuleConcatPlugin = require('webpack/lib/optimize/ModuleConcatenationPlugin');
+var PurifyPlugin = require('@angular-devkit/build-optimizer').PurifyPlugin;
 
-var prodPlugins = [];
-if (process.env.IONIC_ENV === 'prod') {
-  prodPlugins.push(new ModuleConcatPlugin());
+var optimizedProdLoaders = [
+  {
+    test: /\.json$/,
+    loader: 'json-loader'
+  },
+  {
+    test: /\.js$/,
+    loader: [
+      {
+        loader: process.env.IONIC_CACHE_LOADER
+      },
+
+      {
+        loader: '@angular-devkit/build-optimizer/webpack-loader',
+        options: {
+          sourceMap: true
+        }
+      },
+    ]
+  },
+  {
+    test: /\.ts$/,
+    loader: [
+      {
+        loader: process.env.IONIC_CACHE_LOADER
+      },
+
+      {
+        loader: '@angular-devkit/build-optimizer/webpack-loader',
+        options: {
+          sourceMap: true
+        }
+      },
+
+      {
+        loader: process.env.IONIC_WEBPACK_LOADER
+      }
+    ]
+  }
+];
+
+function getProdLoaders() {
+  if (process.env.IONIC_OPTIMIZE_JS === 'true') {
+    return optimizedProdLoaders;
+  }
+  return devConfig.module.loaders;
 }
 
-module.exports = {
+var devConfig = {
   entry: process.env.IONIC_APP_ENTRY_POINT,
   output: {
     path: '{{BUILD}}',
@@ -33,10 +86,6 @@ module.exports = {
       {
         test: /\.ts$/,
         loader: process.env.IONIC_WEBPACK_LOADER
-      },
-      {
-        test: /\.js$/,
-        loader: process.env.IONIC_WEBPACK_TRANSPILE_LOADER
       }
     ]
   },
@@ -48,7 +97,7 @@ module.exports = {
     new webpack.EnvironmentPlugin({
       'IONIC_ENV': JSON.stringify(process.env.IONIC_ENV)
     })
-  ].concat(prodPlugins),
+  ],
 
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
@@ -58,3 +107,49 @@ module.exports = {
     tls: 'empty'
   }
 };
+
+var prodConfig = {
+  entry: process.env.IONIC_APP_ENTRY_POINT,
+  output: {
+    path: '{{BUILD}}',
+    publicPath: 'build/',
+    filename: '[name].js',
+    devtoolModuleFilenameTemplate: ionicWebpackFactory.getSourceMapperFunction(),
+  },
+  devtool: process.env.IONIC_SOURCE_MAP_TYPE,
+
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    modules: [path.resolve('node_modules')]
+  },
+
+  module: {
+    loaders: getProdLoaders()
+  },
+
+  plugins: [
+    ionicWebpackFactory.getIonicEnvironmentPlugin(),
+    ionicWebpackFactory.getCommonChunksPlugin(),
+    new ModuleConcatPlugin(),
+    new PurifyPlugin(),
+    // Get access to IONIC_ENV
+    new webpack.EnvironmentPlugin({
+      'IONIC_ENV': JSON.stringify(process.env.IONIC_ENV)
+    })
+  ],
+
+  // Some libraries import Node modules but don't use them in the browser.
+  // Tell Webpack to provide empty mocks for them so importing them works.
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  }
+};
+
+
+module.exports = {
+  dev: devConfig,
+  prod: prodConfig
+}
+
