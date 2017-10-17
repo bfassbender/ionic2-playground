bcc4f022db001104f81d98f7e687ba1e7cd0d14f
bfassbender <serrin@posteo.de>
2017-10-17 16:06:14 +0200

    Minor improvements on About Page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   templateUrl: 'about.html',
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AboutPage');
+  constructor() {
   }
 
 }
